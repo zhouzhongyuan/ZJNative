44c046ad52d61b3c81c8fdbf3fa5a073ad13eeaf
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-11-30 15:27:05 +0800

    添加要暴露的文件

--- ZJNative/ZJNative.h
+++ ZJNative/ZJNative.h
@@ -15,5 +15,11 @@ FOUNDATION_EXPORT double ZJNativeVersionNumber;
 FOUNDATION_EXPORT const unsigned char ZJNativeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ZJNative/PublicHeader.h>
-
+#import <ZJNative/FirstController.h>
+#import <ZJNative/RootNavigationController.h>
+#import <ZJNative/JudgeCustomerOptional.h>
+#import <ZJNative/Macro.h>
+#import <ZJNative/UIColor+Extension.h>
+#import <ZJNative/CoredataDelegate.h>
+#import <ZJNative/CloseDelegate.h>
 
